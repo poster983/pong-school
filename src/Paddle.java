e6d5a8968a91fdeef3cf35c2fad76b4036bffe68
Joseph Hassell <josephh2018@gmail.com>
2017-05-09 07:48:36 -0500

    Bluej Comp

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,3 +1,5 @@
+package src;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
--- src/Welcome.java
+++ src/Welcome.java
@@ -1,3 +1,5 @@
+package src;
+
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
