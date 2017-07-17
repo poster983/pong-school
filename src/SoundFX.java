46ed510e4fa3f55bb0596285aed6f18528cac648
Joseph Hassell <josephh2018@gmail.com>
2017-07-17 12:42:23 -0500

    Added New Beep

--- src/SoundFX.java
+++ src/SoundFX.java
@@ -15,8 +15,21 @@ public class SoundFX
     public static void CrappyBeep()
     {
         java.awt.Toolkit.getDefaultToolkit().beep();
+        checkOs();
     }
     
+    public static void BettaBeep() {
+    	
+    }
+    
+    public static void checkOs() {
+    	String oz = System.getProperty("os.name");
+    	
+    	if(oz.substring(0, 7).equals("Windows")) {
+    		System.out.println(true);
+    	}
+    	System.out.println(oz.substring(0, 7));
+    }
    /*
    public static void main(String args[]) {
        java.awt.Toolkit.getDefaultToolkit().beep();
